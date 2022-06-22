80e1b1b9ce1e27867a72c7f1240ee55c56024937
ayanjdeka <ayanjdeka@gmail.com>
2022-06-22 03:14:44 -0400

    Added transitions only happen when in view

--- js/main.js
+++ js/main.js
@@ -41,20 +41,96 @@ const observer = new IntersectionObserver(entries => {
   
   observer2.observe(document.querySelector('.circle-wrapper'));
 
+
+
   const observer3 = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-      const triangle = entry.target.querySelector('.triangle');
+      const rectangle = entry.target.querySelector('.rectangle');
   
       if (entry.isIntersecting) {
-        triangle.classList.add('zoom');
+        rectangle.classList.add('exp-desc');
         return;
       }
   
       
-      triangle.classList.remove('zoom');
+      rectangle.classList.remove('exp-desc');
+
     });
   });
   
-  observer3.observe(document.querySelector('.triangle-wrapper'));
+  observer3.observe(document.querySelector('.rectangle-wrapper'));
+
+
+  const observer4 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const rectangle = entry.target.querySelector('.diamond');
+  
+      if (entry.isIntersecting) {
+        rectangle.classList.add('myself-desc');
+        return;
+      }
+  
+      
+      rectangle.classList.remove('myself-desc');
+
+    });
+  });
+  
+  observer4.observe(document.querySelector('.diamond-wrapper'));
+
+  const observer5 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const rhombus = entry.target.querySelector('.rhombus');
+  
+      if (entry.isIntersecting) {
+        rhombus.classList.add('side-desc');
+        return;
+      }
+  
+      
+      rhombus.classList.remove('side-desc');
+
+    });
+  });
+  
+  observer5.observe(document.querySelector('.rhombus-wrapper'));
+
+
+const observer6 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const triangle = entry.target.querySelector('.triangle');
+
+    if (entry.isIntersecting) {
+      triangle.classList.add('conn-desc');
+      return;
+    }
+
+    
+    triangle.classList.remove('conn-desc');
+
+  });
+});
+
+observer6.observe(document.querySelector('.triangle-wrapper'));
+
+
+const observer7 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const star = entry.target.querySelector('.star');
+
+    if (entry.isIntersecting) {
+      star.classList.add('proj-desc');
+      return;
+    }
+
+    
+    star.classList.remove('proj-desc');
+
+  });
+});
+
+observer7.observe(document.querySelector('.star-wrapper'));
+
+
 
   
